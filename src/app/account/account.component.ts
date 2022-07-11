3016dedd4f4ebe0c67d0e782949ee99474dbc9f6
bewu <bewuwy@gmail.com>
2022-07-11 10:29:35 +0200

    list active games on account page

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth, User, onAuthStateChanged } from '@angular/fire/auth';
+import { Firestore, collection, doc, query, where, getDocs } from '@angular/fire/firestore';
 
 
 @Component({
@@ -10,17 +11,30 @@ import { Auth, User, onAuthStateChanged } from '@angular/fire/auth';
 export class AccountComponent implements OnInit {
 
   account : User | null = null;
+  games: any[] = [];
 
-  constructor(private auth: Auth) { }
+  constructor(private auth: Auth, private db: Firestore) { }
 
   ngOnInit(): void { 
     onAuthStateChanged(this.auth, (user) => {
       if (user) {
         this.account = user;
+
+        // get current user's games from firestore
+        const gamesCollection = collection(this.db, 'game');
+        const q = query(gamesCollection, where('players', 'array-contains', user.uid), where("completed", "==", false));
+
+        getDocs(q).then(docs => {
+          for (const doc of docs.docs) {
+            this.games.push(doc.id);
+          }
+        }).catch(err => {
+          console.log(err);
+        });
+
       } else {
         this.account = null;
       }
     });
   }
-
 }
